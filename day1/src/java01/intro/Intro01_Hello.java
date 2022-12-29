a730b1e2fddd0de37a1c378c1a42ff38d809e026
RS <rhoseog@gmail.com>
2022-12-29 13:55:54 +0900

    asdf

--- day1/src/java01/intro/Intro01_Hello.java
+++ day1/src/java01/intro/Intro01_Hello.java
@@ -4,7 +4,7 @@ public class Intro01_Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello SSAFY!!!1111");
+		System.out.println("Hello SSAFY!!!11111111");
 	}
 
 }
